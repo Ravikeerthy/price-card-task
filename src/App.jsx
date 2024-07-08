03d83693455ee4ea252ebb23b19456ad1962d979
Ravikeerthy <ravikeerthy1807@gmail.com>
2024-07-08 23:04:05 +0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import CardComp from './components/CardComp';
+
+const App = () => {
+  const lists = [
+    {
+      title: "FREE",
+      price: "$0/month",
+      user: "single user",
+      user1: true,
+      storage: true,
+      publicProjects: true,
+      communityAccess: true,
+      privateProjects: false,
+      phoneSupport: false,
+      subDomain: false,
+      status: false,
+    },
+    {
+      title: "PLUS",
+      price: "$9/month",
+      user: "5 user",
+      userIcon: "fa-solid fa-check",
+      user1: true,
+      storage: true,
+      publicProjects: true,
+      communityAccess: true,
+      privateProjects: true,
+      phoneSupport: true,
+      subDomain: true,
+      status: false,
+    },
+    {
+      title: "PRO",
+      price: "$49/month",
+      user: "unlimited user",
+      user1: true,
+      storage: true,
+      publicProjects: true,
+      communityAccess: true,
+      privateProjects: true,
+      phoneSupport: true,
+      subDomain: true,
+      status: true,
+    },
+  ];
+  return (
+    <div>
+      <CardComp lists={lists}/>
+    </div>
+  )
+}
+
+export default App
